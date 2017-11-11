33b1d9b023e18310926f39eecd8659d9cb9d1df3
pouriya adibfar <pouriya.adibfar@hotmail.com>
2017-11-11 12:15:33 +0330

    user log fix error null refrence add web config g-zip

--- Netotik.Web/Areas/MyRouter/Controllers/UserManagerController.cs
+++ Netotik.Web/Areas/MyRouter/Controllers/UserManagerController.cs
@@ -1203,6 +1203,10 @@ namespace Netotik.Web.Areas.MyRouter.Controllers
                     user_ip = user.user_ip
                 }).ToList());
             }
+            //if (UsersLogs==null)
+            //{
+
+            //}
             var userReport = UsersLogs.FirstOrDefault();
 
             using (ExcelPackage pck = new ExcelPackage())
@@ -1211,7 +1215,7 @@ namespace Netotik.Web.Areas.MyRouter.Controllers
                 ws.Cells["A1"].LoadFromDataTable(Netotik.Common.Extensions.DataTableExtention.ToDataTable<UserWebsiteLogsWithSessionsModel>(UsersLogs), true, TableStyles.Medium2);
                 Byte[] fileBytes = pck.GetAsByteArray();
                 Response.ClearContent();
-                Response.AddHeader("content-disposition", "attachment;filename=" + userReport != null ? userReport.user : "null" + "_Logs_" + DateTime.Now.ToString("M_dd_yyyy_H_M_s") + ".xlsx");
+                Response.AddHeader("content-disposition", "attachment;filename=" + (userReport != null ? userReport.user : "null" )+ "_Logs_" + DateTime.Now.ToString("M_dd_yyyy_H_M_s") + ".xlsx");
                 Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
                 Response.BinaryWrite(fileBytes);
                 Response.End();
